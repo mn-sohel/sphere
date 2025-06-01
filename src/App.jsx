d2c9fd91aa38744329de47cd6bb2587fe3604f45
mn-sohel <sohel.m0087@gmail.com>
2025-06-01 20:52:04 +0600

    vercel routing permission added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import BlogPage from "./pages/BlogPage";
 
 function App() {
 
   return (
      <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/blog" element={<BlogPage />} />
     </Routes>
     
   )
